12791c1d9d946c9676a30c004b1aeb765eca5e02
Peter Bailis <pbailis@cs.berkeley.edu>
2012-05-18 13:55:45 -0700

    Adding real unit test for PBSPredictor

--- test/unit/org/apache/cassandra/service/PBSPredictorTest.java
+++ test/unit/org/apache/cassandra/service/PBSPredictorTest.java
@@ -18,13 +18,8 @@
  */
 package org.apache.cassandra.service;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.SchemaLoader;
@@ -32,34 +27,84 @@ import org.apache.cassandra.SchemaLoader;
 public class PBSPredictorTest extends SchemaLoader
 {
 
-  private static PBSPredictor predictor = PBSPredictor.instance();
+    private static PBSPredictor predictor = PBSPredictor.instance();
 
-  private void createWriteResponse(double W, double A, String id) 
-  {
-      // Log write response
-      // predictor.logWriteResponse(id); 
-  }
-
-  private void createReadResponse(double R, double S, String id) 
-  {
-      // Log read response
-      // predictor.logReadResponse(id1);
-  }
-
-  @Test
-  public void testDoPrediction()
-  {
-    DatabaseDescriptor.setLogLatenciesForConsistencyPrediction(true);
-    predictor.init();
-
-    predictor.startOperation("id1");
-
-    // Log operations
-    for (int i = 0; i < 10; ++i) {
-      createWriteResponse(10, 10, "id1");
-      createReadResponse(10, 10, "id1");
+    private void createWriteResponse(long W, long A, String id)
+    {
+        predictor.startOperation(id, 0);
+        predictor.logWriteResponse(id, W, W+A);
     }
-    // Verify Prediction
-    PBSPredictionResult result = predictor.doPrediction(3,1,1,10.0f,1, 0.99f); 
-  }
+
+    private void createReadResponse(long R, long S, String id)
+    {
+        predictor.startOperation(id, 0);
+        predictor.logReadResponse(id, R, R+S);
+    }
+
+    @Test
+    public void testDoPrediction()
+    {
+        DatabaseDescriptor.setLogLatenciesForConsistencyPrediction(true);
+        predictor.init(true);
+
+        /*
+            Ensure accuracy given a set of basic latencies
+            Predictions here match a prior Python implementation
+         */
+
+        for (int i = 0; i < 10; ++i)
+        {
+            createWriteResponse(10, 0, String.format("W%d", i));
+            createReadResponse(0, 0, String.format("R%d", i));
+        }
+
+        for (int i = 0; i < 10; ++i)
+        {
+            createWriteResponse(0, 0, String.format("WS%d", i));
+        }
+
+        // 10ms after write
+        PBSPredictionResult result = predictor.doPrediction(2,1,1,10.0f,1, 0.99f);
+
+        assertEquals(1, result.getConsistencyProbability(), 0);
+        assertEquals(2.5, result.getAverageWriteLatency(), .5);
+
+        // 0ms after write
+        result = predictor.doPrediction(2,1,1,0f,1, 0.99f);
+
+        assertEquals(.75, result.getConsistencyProbability(), 0.05);
+
+        // k=5 versions staleness
+        result = predictor.doPrediction(2,1,1,5.0f,5, 0.99f);
+        assertEquals(.98, result.getConsistencyProbability(), 0.05);
+        assertEquals(2.5, result.getAverageWriteLatency(), .5);
+
+        for (int i = 0; i < 10; ++i)
+        {
+            createWriteResponse(20, 0, String.format("WL%d", i));
+        }
+
+        // 5ms after write
+        result = predictor.doPrediction(2,1,1,5.0f,1, 0.99f);
+
+        assertEquals(.67, result.getConsistencyProbability(), .05);
+
+        // N = 5
+        result = predictor.doPrediction(5,1,1,5.0f,1, 0.99f);
+
+        assertEquals(.42, result.getConsistencyProbability(), .05);
+        assertEquals(1.33, result.getAverageWriteLatency(), .5);
+
+        for (int i = 0; i < 10; ++i)
+        {
+            createWriteResponse(100, 100, String.format("WVL%d", i));
+            createReadResponse(100, 100, String.format("RL%d", i));
+        }
+
+        result = predictor.doPrediction(2,1,1,0f,1, 0.99f);
+
+        assertEquals(.808, result.getConsistencyProbability(), .05);
+        assertEquals(26.5, result.getAverageWriteLatency(), 1);
+        assertEquals(62.2, result.getAverageReadLatency(), 1);
+    }
 }
